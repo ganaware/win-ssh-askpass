ba3c75fad1ef71518ea8c9f26956766f6157f2eb
Nayuta Taga <nayuta@ganaware.jp>
2011-11-02 12:51:15 +0900

    rename the project name from win-ssh-askpass to win-ssh-agent

--- agentrc.h
+++
@@ -1,4 +0,0 @@
-#define IDR_TASKTRAY                    101
-#define IDI_TASKTRAY                    102
-#define ID_MENUITEM_Exit                40001
-#define ID_MENUITEM_Add                 40002
--- askpassrc.h
+++
@@ -1,4 +0,0 @@
-#define IDI_ASK_PASS                    101
-#define IDD_ASK_PASS                    102
-#define IDC_PASSPHRASE                  1001
-#define IDC_PROMPT                      1002
